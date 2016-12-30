5b3fe15067aa52925156fc79dd8783227bc02e1a
Fatalstrikes <fatalstrikes@gmail.com>
2016-12-29 22:16:00 -0800

    Counter bar finished

--- components/facebook-media-object.jsx
+++ components/facebook-media-object.jsx
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react'
 import { styled } from 'styletron-react'
 
-const MediaObjectContainer = styled('div', {
+const MediaObjectContainer = styled ('div', {
   boxShadow: '0px 1px 6px #888888',
   fontFamily: 'sans-serif',
   paddingTop: '.5rem',
@@ -15,7 +15,6 @@ const MediaObjectHeader = styled('div', {
   marginLeft: '1rem',
   marginRight: '1rem',
 })
-
 
 const MediaObjectHeaderAvatar = styled('img', {
   height: "2.5rem",
@@ -31,7 +30,7 @@ const MediaObjectHeaderData = styled('div', {
   flexDirection: 'column',
   marginBottom: '.5rem',
   marginTop: '.5rem',
-  flexGrow: '2'
+  flexGrow: '2',
 })
 
 const MediaObjectHeaderDataName = styled('span', {
@@ -58,26 +57,120 @@ const MediaObjectActionButton = styled('span', {
   height: '1.5rem',
 })
 
+const MediaObjectPost = styled('div', {
+  marginBottom: '.5rem',
+  marginLeft: '1rem',
+  marginRight: '1rem',
+  marginTop: '.5rem',
+  // maxHeight: '4rem',
+  // lineHeight: '1.25rem',
+  // overflow: 'hidden',
+  textAlign: 'justify',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'no-wrap',
+})
 
-const FacebookMediaObject = (props) => {
+const MediaObjectPostMedia = styled('div', {
+  marginBottom: '.5rem',
+  marginTop: '.5rem',
+})
+
+const MediaObjectCounts = styled('div', {
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  marginBottom: '.5rem',
+  marginTop: '.5rem',
+  paddingBottom: '.25rem',
+  paddingLeft: '.5rem',
+  paddingTop: '.25rem',
+})
+
+const MediaObjectIcon = styled('span', (props) => ({
+  borderStyle: 'solid',
+  borderWidth: '1px',
+  height: props.height,
+  width: props.width,
+}))
+
+const PullLeft = styled('div', {
+  marginRight: '.5rem',
+  marginLeft: '.5rem',
+})
+
+const PullRight = styled('div', {
+  marginLeft: 'auto',
+  marginRight: '.5rem',
+})
+
+const MediaObjectCountsCounters = styled('span', {
+  color: '#888888',
+  fontSize: '.75rem',
+  fontWeight: '500',
+  marginRight: '.25rem',
+  marginLeft: '.25rem',
+})
+
+const countFormatter = (count) => {
+  return count > 1000000 ? count/1000000 + "M" : count > 1000 ? count/1000 + "K" : count
+}
+
+const FacebookMediaObject = ({avatar, name, timestamp, icon, post, children, reactionsCount, commentsCount, sharesCount}) => {
+
   return (
     <MediaObjectContainer>
-        <MediaObjectHeader>
-          {props.avatar && <MediaObjectHeaderAvatar src={props.avatar} alt="facebook avatar"/>}
-          <MediaObjectHeaderData>
-            <MediaObjectHeaderDataName>{props.name}</MediaObjectHeaderDataName>
-            <MediaObjectHeaderDataTimestamp>{props.timestamp}</MediaObjectHeaderDataTimestamp>
-          </MediaObjectHeaderData>
-          <MediaObjectActionButton><i>{props.icon}</i></MediaObjectActionButton>
-        </MediaObjectHeader>
+
+      { /* *** *** MO Header *** *** */}
+      <MediaObjectHeader>
+        {avatar && <MediaObjectHeaderAvatar src={avatar} alt="facebook avatar"/>}
+        <MediaObjectHeaderData>
+          <MediaObjectHeaderDataName>{name}</MediaObjectHeaderDataName>
+          <MediaObjectHeaderDataTimestamp>{timestamp}</MediaObjectHeaderDataTimestamp>
+        </MediaObjectHeaderData>
+        <MediaObjectActionButton><i>{icon}</i></MediaObjectActionButton>
+      </MediaObjectHeader>
+      { /* <<= =<< MO Header >>= ==> */ }
+
+      { /* *** *** MO Post Content *** *** */}
+      {post && <MediaObjectPost>{post}</MediaObjectPost>}
+      {children && <MediaObjectPostMedia>
+        {children}
+      </MediaObjectPostMedia>}
+      { /* <<= =<< MO Post Content >>= ==> */ }
+
+      { /* *** *** MO Counters *** *** */}
+      <MediaObjectCounts>
+        <PullLeft>
+          <MediaObjectIcon height="1rem" width="1rem"/>
+          <MediaObjectCountsCounters>{countFormatter(reactionsCount)}</MediaObjectCountsCounters>
+        </PullLeft>
+        <PullRight>
+          <MediaObjectCountsCounters>{countFormatter(commentsCount)} Comments</MediaObjectCountsCounters>
+          <MediaObjectCountsCounters>{countFormatter(sharesCount)} Shares</MediaObjectCountsCounters>
+        </PullRight>
+      </MediaObjectCounts>
+      { /* <<= =<< MO Counters >>= ==> */ }
+
+
     </MediaObjectContainer>
   )
 }
 
 FacebookMediaObject.propTypes = {
   avatar: PropTypes.string,
+  children: PropTypes.element,
+  commentsCount: PropTypes.number.isRequired,
+  sharesCount: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
+  post: PropTypes.string,
   timestamp: PropTypes.string.isRequired,
+  reactionsCount: PropTypes.number.isRequired,
+}
+
+FacebookMediaObject.defaultProps = {
+  commentsCount: 0,
+  sharesCount: 0,
+  reactionsCount: 0,
 }
 
 export default FacebookMediaObject
--- stories/index.js
+++ stories/index.js
@@ -12,11 +12,6 @@ document.head.appendChild(styleSheet);
 const styletron = new Styletron([styleSheet]);
 
 storiesOf('Facebook Media Object', module)
-  .add('empty', () => (
-    <StyletronProvider styletron={styletron}>
-      <FacebookMediaObject />
-    </StyletronProvider>
-  ))
   .add('with header no image', () => (
     <StyletronProvider styletron={styletron}>
       <FacebookMediaObject
@@ -32,5 +27,50 @@ storiesOf('Facebook Media Object', module)
         name="Fill Murray"
         timestamp="post 3hrs ago"
         />
+    </StyletronProvider>
+  ))
+  .add('with header and content no media', () => (
+    <StyletronProvider styletron={styletron}>
+      <FacebookMediaObject
+        avatar="http://fillmurray.com/200/200"
+        name="Fill Murray"
+        timestamp="post 3hrs ago"
+        post="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        />
+    </StyletronProvider>
+  ))
+  .add('with header and content and media', () => (
+    <StyletronProvider styletron={styletron}>
+      <FacebookMediaObject
+        avatar="http://fillmurray.com/200/200"
+        name="Fill Murray"
+        timestamp="post 3hrs ago"
+        post="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        >
+        <img src="http://fillmurray.com/800/600" width="100%"/>
+    </FacebookMediaObject>
+    </StyletronProvider>
+  ))
+  .add('with header and media no content', () => (
+    <StyletronProvider styletron={styletron}>
+      <FacebookMediaObject
+        avatar="http://fillmurray.com/200/200"
+        name="Fill Murray"
+        timestamp="post 3hrs ago"        >
+        <img src="http://fillmurray.com/800/600" width="100%"/>
+    </FacebookMediaObject>
+    </StyletronProvider>
+  ))
+  .add('with header and footer', () => (
+    <StyletronProvider styletron={styletron}>
+      <FacebookMediaObject
+        avatar="http://fillmurray.com/200/200"
+        name="Fill Murray"
+        timestamp="post 3hrs ago"
+        reactionsCount={3300000}
+        commentsCount={220}
+        sharesCount={2200} >
+        <img src="http://fillmurray.com/800/600" width="100%"/>
+    </FacebookMediaObject>
     </StyletronProvider>
   ))
